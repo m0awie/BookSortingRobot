29fdc5472d17f4ef27178b61f5e957ca022c97ca
m0awie <mowiehaff@gmail.com>
2024-10-23 23:47:44 +1100

    arduino code start

--- gripper/gripper.ino
+++ gripper/gripper.ino
@@ -1,9 +1,23 @@
 #include <servo.h>
 
+Servo myServo;
+int servoPin {9};
+
 void setup() {
-    
+  // open the serial port:
+  Serial.begin(9600);   // baud rate
+  myServo.atatch(servoPin);
 }
 
 void loop() {
+    int pos_deg{0};
+    // check for incoming serial data:
+    if (Serial.available() > 0) {
+        // read incoming serial data:
+        float width = Serial.read();
+        pos_deg = width; // fix this
+        
 
+    }
+    myServo.write(pos_deg);
 }
