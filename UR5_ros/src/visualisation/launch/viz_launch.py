7312672f2e22f684a5652b43037ab8f9e4b55ba2
m0awie <mowiehaff@gmail.com>
2024-11-07 14:06:22 +1100

    setting up rviz

--- UR5_ros/src/visualisation/launch/viz_launch.py
+++ UR5_ros/src/visualisation/launch/viz_launch.py
@@ -1,43 +1,44 @@
-# gripper_launch.py
 from launch import LaunchDescription
-from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
 import os
+import xacro
 
 def generate_launch_description():
-    urdf_file = os.path.join(
-        get_package_share_directory('visualisation'),
-        'urdf',
-        'gripper.urdf.xacro',
-        'ur5_with_gripper.urdf.xacro'
-    )
-    rviz_config = os.path.join(
-        get_package_share_directory('visualisation'),
-        'rviz',
-        'view_gripper.rviz'
-    )
-    
+    # Declare path to the URDF file
+    package_name = 'visualisation'
+    xacro_path = 'urdf/ur5_with_gripper.xacro'
+    rviz_path = 'rviz/display.rviz'
+
+
+    xacro_file = os.path.join(get_package_share_directory(package_name), xacro_path)
+    xacro_raw_description = xacro.process_file(xacro_file).toxml()
+
+    rviz_file = os.path.join(get_package_share_directory(package_name), rviz_path)
+
     return LaunchDescription([
-        # Gripper control node
-        Node(
-            package='visualisation',
-            executable='visualisation',
-            name='gripper_viz',
-            output='screen'
-        ),
-        # Robot State Publisher
+        # Robot description parameter
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
-            arguments=[urdf_file],
+            output='screen',
+            parameters=[{'robot_description': xacro_raw_description, 'source_list':['main_joint']}]
+        ),
+
+        # Joint State Publisher (using GUI for easy control)
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher',
             output='screen'
         ),
-        # RViz node
+
+        # RViz for visualization
         Node(
             package='rviz2',
             executable='rviz2',
             name='rviz2',
-            arguments=['-d', rviz_config],
+            arguments=['-d', rviz_file],
             output='screen'
-        ),
+        )
     ])
--- UR5_ros/src/visualisation/setup.py
+++ UR5_ros/src/visualisation/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'visualisation'
 
@@ -7,9 +9,17 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
+        # Essential package files
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        
+        # Include launch files
+        (os.path.join('share', package_name, 'launch'), glob('launch/*_launch.py')),
+
+        # Include RViz and URDF files, if present
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.xacro')),
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -19,8 +29,6 @@ setup(
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': [
-            "visualisation = visualisation.GripperVisualisation:main"
-        ],
+        'console_scripts': [],
     },
 )
